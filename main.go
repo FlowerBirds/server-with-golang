46e0e0b2fdd51d974096ccd73a7b6cf51b532862
FlowBrids <eabour@163.com>
2022-01-03 19:38:13 +0800

    修改Path为PathPrefix，解决静态文件无法访问问题

--- main.go
+++ main.go
@@ -3,16 +3,71 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-func main() {
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
 
+func NewResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{w, http.StatusOK}
+}
+
+var totalRequests = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_request_total",
+		Help: "Number of Requests",
+	}, []string{"path"},
+)
+var responseStatus = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "response_status",
+		Help: "Status of HTTP response",
+	},
+	[]string{"status"},
+)
+var httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	Name: "http_response_time_seconds",
+	Help: "Duration of HTTP requests.",
+}, []string{"path"})
+
+func prometheusMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		route := mux.CurrentRoute(request)
+		path, _ := route.GetPathTemplate()
+		log.Printf("visit path: %s", path)
+
+		timer := prometheus.NewTimer(httpDuration.WithLabelValues(path))
+		rw := NewResponseWriter(writer)
+		next.ServeHTTP(rw, request)
+
+		statusCode := rw.statusCode
+		responseStatus.WithLabelValues(strconv.Itoa(statusCode)).Inc()
+		totalRequests.WithLabelValues(path).Inc()
+		timer.ObserveDuration()
+
+	})
+}
+
+func init() {
+	prometheus.Register(totalRequests)
+	prometheus.Register(responseStatus)
+	prometheus.Register(httpDuration)
+}
+
+func main() {
 	router := mux.NewRouter()
-	router.Path("/").Handler(http.FileServer(http.Dir("./static/")))
-	router.Path("/router").Handler(promhttp.Handler())
+	router.Use(prometheusMiddleware)
+
+	router.Path("/metrics").Handler(promhttp.Handler())
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 
 	fmt.Println("Serving requests on port 9000")
 	err := http.ListenAndServe(":9000", router)
